e4a861dab9ed5d5287c5649fcfb000263725a933
mitsuhori_yu <mitsuhori_y@mti.co.jp>
2017-09-25 16:09:06 +0900

    facebookボタン表示

---
+++ app/src/main/java/jp/co/flexapp/presentation/activity/FbOAuthActivity.kt
@@ -0,0 +1,14 @@
+package jp.co.flexapp.presentation.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import jp.co.flexapp.R
+
+class FbOAuthActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
--- app/src/main/java/jp/co/flexapp/presentation/fragment/FbPageFragment.java
+++ app/src/main/java/jp/co/flexapp/presentation/fragment/FbPageFragment.java
@@ -1,71 +1,66 @@
 package jp.co.flexapp.presentation.fragment;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.ProfileTracker;
+import com.facebook.login.widget.LoginButton;
+
+import java.net.URL;
+
 import jp.co.flexapp.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link FbPageFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link FbPageFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class FbPageFragment extends BasePageFragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private CallbackManager callbackManager;
+    private AccessTokenTracker accessTokenTracker;
+    private ProfileTracker profileTracker;
+    private LoginButton loginButton;
+    private String firstName, lastName, email, birthday, gender;
+    private URL profilePicture;
+    private String userId;
+    private String TAG = "LoginActivity";
 
     private OnFragmentInteractionListener mListener;
 
     public FbPageFragment() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FbPageFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static FbPageFragment newInstance(String param1, String param2) {
         FbPageFragment fragment = new FbPageFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        FacebookSdk.sdkInitialize(this.getActivity());
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fb_page, container, false);
+        View view = inflater.inflate(R.layout.fragment_fb_page, container, false);
+        callbackManager = CallbackManager.Factory.create();
+        loginButton = (LoginButton) view.findViewById(R.id.fb_login_button);
+        loginButtonInitialize();
+        return view;
+    }
+
+    private void loginButtonInitialize() {
+        loginButton.setHeight(100);
+        loginButton.setTextColor(Color.WHITE);
+        loginButton.setCompoundDrawablesWithIntrinsicBounds(null, null, null, null);
+        loginButton.setCompoundDrawablePadding(0);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
